6ac2cbce03e3b0bda7aa392df26be1919eca95e1
dzhumanrm <dzhumanrm@gmail.com>
2021-09-23 12:33:59 +0300

    - update page Setup phrase

--- src/ui/components/CreateAccount/CreateAccount.jsx
+++ src/ui/components/CreateAccount/CreateAccount.jsx
@@ -1,8 +1,11 @@
-import { Typography, Button } from '@material-ui/core';
+import React from "react";
+import { Typography, Button, Box, Paper, Grid } from '@material-ui/core';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
 import { useStoreActions } from 'easy-peasy';
 import { generateMnemonic } from 'bip39-light';
 import { Footer } from '../Footer/Footer';
 import { useStyles } from './CreateAccount.styles';
+
 
 export const CreateAccount = () => {
   const onCreateUser = useStoreActions((actions) => actions.general.user.onCreateUser);
@@ -10,26 +13,66 @@ export const CreateAccount = () => {
 
   const mnemonic = generateMnemonic();
   const createUser = () => onCreateUser({ mnemonic });
+  const copyPhrase = async () => {
+    try {
+      await navigator.clipboard.writeText(mnemonic);
+    } catch (e) {
+      console.log('Error:', e);
+    }
+
+  }
 
   return (
     <>
       <div className={classes.container}>
-        <div className={classes.content}>
+        <Box maxWidth={820} className={classes.content}>
+          <Box display="flex" m={2}>
+            <img src="../images/key.png" alt="" />
+          </Box>
           <Typography variant="h1" color="textPrimary" className={classes.header}>
-            Create Linkdrop Account
+            Setup Phrase
           </Typography>
-          <Typography variant="subtitle1" color="textSecondary" className={classes.title}>
-            Mnemonic phrase - {mnemonic}
-          </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={createUser}
-          >
-            Create Account
-          </Button>
-        </div>
+          <Box maxWidth={592}>
+            <Typography variant="body1" color="textSecondary" className={classes.subHeader}>
+              Copy or write down the following words in the exact order listed below. You are not
+              able to recover your <strong className={classes.strong}>Linkdrop account</strong> without them
+            </Typography>
+          </Box>
+          <Box display='flex' alignItems="center">
+            <Grid container>
+
+            {
+              mnemonic ? mnemonic.split(' ').map((item, index)=>(
+                  <Grid item lg={2} md={2} xs={6} key={item}>
+                    <Paper elevation={0} className={classes.paper}>
+                      {item} #{index+1}
+                    </Paper>
+                  </Grid>
+                )
+              ): null
+            }
+            </Grid>
+          </Box>
+          <Box display="flex" className={classes.responsiveDirection}>
+            <Button
+              variant="outlined"
+              color="primary"
+              className={classes.button}
+              startIcon={<FileCopyOutlinedIcon/>}
+              onClick={copyPhrase}
+            >
+              Copy Phrase
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              onClick={createUser}
+            >
+              Next
+            </Button>
+          </Box>
+        </Box>
       </div>
       <Footer />
     </>
--- src/ui/components/CreateAccount/CreateAccount.styles.js
+++ src/ui/components/CreateAccount/CreateAccount.styles.js
@@ -1,11 +1,13 @@
 import { makeStyles } from '@material-ui/core';
+import {theme} from "../../config/theme";
 
 const styles = {
   container: {
-    height: 'calc(100vh - 73px - 52px - 50px)', // caution + topbar + footer
+    minHeight: 'calc(100vh - 73px - 52px - 50px)', // caution + topbar + footer
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#F7F8FA',
   },
   content: {
     display: 'flex',
@@ -20,15 +22,43 @@ const styles = {
     fontWeight: 700,
     letterSpacing: 0.25,
     marginTop: 8,
+    marginBottom: theme.spacing(2),
+  },
+  subHeader: {
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: 400,
+    lineHeight: '22px',
+    letterSpacing: '0px',
+    textAlign: 'center',
+    marginBottom: theme.spacing(2),
   },
   title: {
     marginTop: 16,
     letterSpacing: 0.5,
   },
+  strong: {
+    color: '#212121',
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: '#C9C9C9',
+    whiteSpace: 'nowrap',
+    margin: theme.spacing(0.5),
+  },
   button: {
     marginTop: 24,
-    padding: '16px 35px',
+    borderRadius: theme.spacing(1),
+    padding: '11px 35px',
     fontWeight: 500,
+    minWidth: 170,
+    marginRight: theme.spacing(2),
+  },
+  [theme.breakpoints.down('xs')]: {
+    responsiveDirection: {
+      flexDirection: 'column-reverse',
+    },
   },
 };
 
