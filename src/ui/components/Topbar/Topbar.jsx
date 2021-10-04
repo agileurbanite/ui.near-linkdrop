02d75088b2140e78e26e686b8fc5a5727b1d7952
eclipseeer <eclipseeer@gmail.com>
2021-10-04 10:41:59 +0300

    Remove network name from 'NEAR Linkdrop' title

--- src/ui/components/Topbar/Topbar.jsx
+++ src/ui/components/Topbar/Topbar.jsx
@@ -3,7 +3,6 @@ import { useStoreState } from 'easy-peasy';
 import { LinearProgress } from '@material-ui/core';
 import { Account } from './Account/Account';
 import { routes } from '../../../config/routes';
-import { appName } from '../../config/appName';
 import { useStyles } from './Topbar.styles';
 
 export const Topbar = () => {
@@ -15,7 +14,7 @@ export const Topbar = () => {
     <>
       <div className={classes.container}>
         <span className={classes.logo}>
-          <Link to={routes.campaigns}>{appName}</Link>
+          <Link to={routes.campaigns}>NEAR Linkdrop</Link>
         </span>
         <div className={classes.account}>{accountId && <Account accountId={accountId} />}</div>
       </div>
