623b8b9dc9422e843428c2061796c178814b2611
eclipseeer <eclipseeer@gmail.com>
2021-10-06 12:31:04 +0300

    Add logo and favicon

--- src/ui/components/Topbar/Topbar.jsx
+++ src/ui/components/Topbar/Topbar.jsx
@@ -4,6 +4,7 @@ import { LinearProgress } from '@material-ui/core';
 import { Account } from './Account/Account';
 import { routes } from '../../../config/routes';
 import { useStyles } from './Topbar.styles';
+import logo from '../../images/linkdrop-logo.png';
 
 export const Topbar = () => {
   const accountId = useStoreState((store) => store.general.user.wallet.accountId);
@@ -13,9 +14,14 @@ export const Topbar = () => {
   return (
     <>
       <div className={classes.container}>
-        <span className={classes.logo}>
-          <Link to={routes.campaigns}>NEAR Linkdrop</Link>
-        </span>
+        <div className={classes.logo}>
+          <Link to={routes.campaigns}>
+            <div className={classes.logoWrapper}>
+              <img src={logo} alt="linkdrop logo (chain links)" />
+              NEAR Linkdrop
+            </div>
+          </Link>
+        </div>
         <div className={classes.account}>{accountId && <Account accountId={accountId} />}</div>
       </div>
       {isLoading && <LinearProgress className={classes.progress} />}
