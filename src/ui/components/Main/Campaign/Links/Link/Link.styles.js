cdd428d90fdd38d7abbb42ab1439d825354aceca
eclipseeer <eclipseeer@gmail.com>
2021-10-20 12:20:08 +0300

    Remove checkboxes, add export links

--- src/ui/components/Main/Campaign/Links/Link/Link.styles.js
+++ src/ui/components/Main/Campaign/Links/Link/Link.styles.js
@@ -4,17 +4,13 @@ const styles = (theme) => ({
   container: {
     height: 50,
     display: 'grid',
-    gridTemplateColumns: '42px 60px auto 130px 24px 6px 24px 6px 24px',
+    gridTemplateColumns: '60px auto 130px 24px 6px 24px 6px 24px',
     gridTemplateRows: '100%',
     gridTemplateAreas: `
-      'a b c d g . e . f'
+      'b c d g . e . f'
     `,
     alignItems: 'center',
     color: (isActive) => (isActive ? theme.palette.text.primary : theme.palette.text.disabled),
-  },
-  checkbox: {
-    gridArea: 'a',
-    justifySelf: 'start',
   },
   order: {
     gridArea: 'b',
