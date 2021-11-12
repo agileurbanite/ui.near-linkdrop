f848308888261f0b01cdec75e46713296706c420
eclipseeer <eclipseeer@gmail.com>
2021-11-12 13:48:12 +0200

    Update metadata / add social media

---
+++ src/config/references.js
@@ -0,0 +1,5 @@
+export const socialMedia = {
+  twitter: 'https://twitter.com/NEARDR0P',
+  telegram: 'https://t.me/joinchat/sPqlT3Zt-mAyNmI0',
+  gitHub: 'https://github.com/NEAR-labs/contracts.near-linkdrop'
+}
--- src/ui/components/Footer/Footer.jsx
+++ src/ui/components/Footer/Footer.jsx
@@ -1,11 +1,17 @@
 import cn from 'classnames';
+import { Twitter, Telegram, GitHub } from '@material-ui/icons';
+import { SocialMedia } from './SocialMedia/SocialMedia';
+import { socialMedia } from '../../../config/references';
 import { useStyles } from './Footer.styles';
 
-export const Footer = ({ classNames, variant }) => {
-  const classes = useStyles({ variant });
+export const Footer = ({ classNames }) => {
+  const classes = useStyles();
   return (
     <div className={cn(classes.container, classNames?.container)}>
-      <p className={classes.text}>©2021 NEARDROP</p>
+      <span className={classes.text}>©2021 NEARDROP</span>
+      <SocialMedia icon={Twitter} name="Twitter" href={socialMedia.twitter} />
+      <SocialMedia icon={Telegram} name="Telegram" href={socialMedia.telegram} />
+      <SocialMedia icon={GitHub} name="GitHub" href={socialMedia.gitHub} />
     </div>
   );
 };
