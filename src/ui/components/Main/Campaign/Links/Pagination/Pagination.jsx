6aec8321c7e875ec310176a5c6b0b4a870f4d449
Boniface <sollomon35@gmail.com>
2021-12-16 13:01:06 +0200

    Refactoring pagination

--- src/ui/components/Main/Campaign/Links/Pagination/Pagination.jsx
+++ src/ui/components/Main/Campaign/Links/Pagination/Pagination.jsx
@@ -8,34 +8,31 @@ import { PaginationItem } from './PaginationItem/PaginationItem';
 export const Pagination = () => {
   const pagination = useStoreState((state) => state.campaigns.campaign.pagination);
   const onLoadKeys = useStoreActions((actions) => actions.campaigns.onLoadKeys);
-  const [loader, setLoader] = useState(null);
+  const [loader, setLoader] = useState(false);
   const classes = useStyles();
-  const { total, elementsPerPage } = pagination;
-  const countPage = Math.round(total / elementsPerPage);
 
+  const { lastPage, total, elementsPerPage } = pagination;
 
   const handleChange = (_, page) => {
     onLoadKeys({
       page,
       total,
       elementsPerPage,
-      showLoader: () => setLoader('loading'),
-      hideLoader: () => setLoader(null),
+      showLoader: () => setLoader(true),
+      hideLoader: () => setLoader(false),
     });
   };
 
   return (
     <div className={classes.container}>
-      {total < elementsPerPage ? (
-        <div className={classes.container} />
-      ) : (
+      {total > elementsPerPage && (
         <MuiPagination
           renderItem={(item) => <PaginationItem item={item} loader={loader} />}
           onChange={handleChange}
-          count={countPage}
+          count={lastPage}
           color="primary"
           siblingCount={0}
-          disabled={loader === 'loading'}
+          disabled={loader}
         />
       )}
     </div>
