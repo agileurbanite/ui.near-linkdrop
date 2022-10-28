7442ae05f06d6930a810b32420ca4c53fe435ae3
Patrick Woo <agileurbanite@gmail.com>
2022-10-27 22:54:15 -0700

    fix: update gh url

--- src/config/references.js
+++ src/config/references.js
@@ -1,5 +1,5 @@
 export const socialMedia = {
   twitter: 'https://twitter.com/NEARDR0P',
   telegram: 'https://t.me/joinchat/sPqlT3Zt-mAyNmI0',
-  gitHub: 'https://github.com/NEAR-labs/contracts.near-linkdrop'
+  gitHub: 'https://github.com/agileurbanite/contracts.near-linkdrop'
 }
